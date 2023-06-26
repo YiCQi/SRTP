6636c69f51517e102a76754a54c2b1977d85724d
YiCQi <123134969+YiCQi@users.noreply.github.com>
2023-06-26 10:57:04 +0800

    Update prefetch_fastqgz.sh

--- pipeline/prefetch_fastqgz.sh
+++ pipeline/prefetch_fastqgz.sh
@@ -1,1 +1,18 @@
 # prefetch and turn-to-fastq.gz
+
+projects=$(cat projects_list.txt)
+for project in $projects
+do
+    echo $project
+    mkdir "${project}"
+    mkdir "${project}"/01_sra
+    prefetch $project -O ./"${project}"/01_sra
+
+    mkdir "${project}"/02_fastq_gz
+    for i in ./"${project}"/01_sra/SRR*;do fastq-dump.3.0.0 ${i}/*sra --split-files --gzip --outdir ./"${project}/02_fastq_gz" & done
+
+    # mkdir "${project}"/02_fastq
+    # for i in ./"${project}"/01_sra/SRR*;do fastq-dump.3.0.0 ${i}/*sra --split-files --outdir ./"${project}/02_fastq" & done
+
+    rm -rf "${project}"/01_sra
+done
